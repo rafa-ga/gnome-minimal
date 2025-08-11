3fd98c69f92f6e32769e9f049aaa290356254afc
n40k1 <41051597+rafa-ga@users.noreply.github.com>
2025-08-11 13:42:00 +0200

    Update arch_tew_post_1.sh

--- scripts/arch_tew_post_1.sh
+++ scripts/arch_tew_post_1.sh
@@ -25,6 +25,11 @@ source "$HOME/.bashrc"
 echo -e "\n░▒▓█ ALIASES AÑADIDOS. █▓▒░\n"
 sleep 3
 
+mkdir "$HOME/.icons"
+tar -xf "/opt/gnome-minimal/icons/Reversal.tar.xz" -C "$HOME/.icons"
+rm -rf "/opt/gnome-minimal/icons"
+
+gsettings set org.gnome.desktop.interface icon-theme Reversal
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 gsettings set org.gnome.desktop.peripherals.keyboard delay 250
 
@@ -51,17 +56,19 @@ echo -e "\n░▒▓█ INSTALADO 'gdm-settings'. █▓▒░\n"
 echo -e "\n░▒▓█ INSTALADO 'gnome-shell-extension-installer'. █▓▒░\n"
 sleep 3
 
-gnome-shell-extension-installer 307 3210 4269 5219 7065 7535 --yes
+gnome-shell-extension-installer 307 3210 4269 5219 7065 --yes
 
 echo -e "\n░▒▓█ INSTALADO 'Dash to Dock'. █▓▒░\n"
 echo -e "\n░▒▓█ INSTALADO 'Compiz windows effect'. █▓▒░\n"
 echo -e "\n░▒▓█ INSTALADO 'Alphabetical App Grid'. █▓▒░\n"
 echo -e "\n░▒▓█ INSTALADO 'TopHat'. █▓▒░\n"
 echo -e "\n░▒▓█ INSTALADO 'Tiling Shell'. █▓▒░\n"
-echo -e "\n░▒▓█ INSTALADO 'Accent Icons'. █▓▒░\n"
 sleep 3
 
-sudo pacman -Rns --noconfirm $(pacman -Qdtq)
+yay -Rns --noconfirm gnome-shell-extension-installer
+echo -e "\n░▒▓█ DESINSTALADO 'gnome-shell-extension-installer'. █▓▒░\n"
+
+yay -Rns $(pacman -Qdtq) --noconfirm
 echo -e "\n░▒▓█ DEPENDENCIAS HUÉRFANAS ELIMINADAS. █▓▒░\n"
 
 echo -e "\n░▒▓█ REINICIANDO EN 3... █▓▒░\n"
