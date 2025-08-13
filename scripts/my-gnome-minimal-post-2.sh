7a0f8ba0b1a3c38bc80cce98d449b208b1f9ef8c
n40k1 <41051597+rafa-ga@users.noreply.github.com>
2025-08-13 10:02:40 +0200

    Add files via upload

---
+++ scripts/my-gnome-minimal-post-2.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+set -e
+
+dconf load /org/gnome/nautilus/ < "/opt/gnome-minimal/configuracion/nautilus.conf"
+echo -e "\n░▒▓█ 'Nautilus' CONFIGURADO. █▓▒░\n"; sleep 3
+
+dconf load /org/gnome/desktop/app-folders/ < "/opt/gnome-minimal/configuracion/app-folders.conf"
+echo -e "\n░▒▓█ DASHBOARD CONFIGURADO. █▓▒░\n"; sleep 3
+
+EXT_DIR="$HOME/.local/share/gnome-shell/extensions"
+
+for ext in "$EXT_DIR"/*; do
+    SCHEMAS_DIR="$ext/schemas"
+
+    if [[ ! -f "$SCHEMAS_DIR/gschemas.compiled" ]]; then
+        glib-compile-schemas "$SCHEMAS_DIR"
+        gnome-extensions enable $(basename "$ext")
+        echo -e "\n░▒▓█ ACTIVADO '$(basename "$ext")'. █▓▒░\n"
+    else
+        gnome-extensions enable $(basename "$ext")
+        echo -e "\n░▒▓█ ACTIVADO '$(basename "$ext")'. █▓▒░\n"
+    fi
+done
+
+sleep 3
+
+dconf load /org/gnome/shell/ < "/opt/gnome-minimal/configuracion/gnome-shell.conf"
+echo -e "\n░▒▓█ EXTENSIONES CONFIGURADAS. █▓▒░\n"; sleep 3
+
+echo "¿QUIERES INSTALAR ALGUNAS DE LAS SIGUIENTES APLICACIONES PARA GAMING?"
+echo "0) Ninguna."
+echo "1) Steam."
+echo "2) Heroic."
+echo "3) Lutris."
+echo "3) ProtonPlus."
+echo "4) Discord."
+read -rp  "PUEDES ELEGIR VARIAS OPCIONES (SEPARADAS POR ESPACIOS): " opciones
+echo
+
+for i in $opciones; do
+    case "$i" in
+        0)
+            echo -e "░▒▓█ NO SE INSTALARÁ NINGUNA APLICACIÓN █▓▒░\n"
+            break
+            ;;
+        1)
+            echo -e "░▒▓█ INSTALANDO 'Steam'... █▓▒░\n"
+            pacman -Syu --noconfirm steam
+            ;;
+        2)
+            echo -e "░▒▓█ INSTALANDO 'Heroic Games Launcher'... █▓▒░\n"
+            yay -S --noconfirm heroic-games-launcher
+            ;;
+        3)
+            echo -e "░▒▓█ INSTALANDO 'Lutris'... █▓▒░\n"
+            pacman -Syu --noconfirm lutris
+            ;;
+        4)
+            echo -e "░▒▓█ INSTALANDO 'ProtonPlus'... █▓▒░\n"
+            yay -S --noconfirm protonplus
+            ;;
+        5)
+            echo -e "░▒▓█ INSTALANDO 'Discord'... █▓▒░\n"
+            pacman -Syu --noconfirm discord
+            ;;
+        *)
+            echo "░▒▓█ '$i' NO ES UNA OPCIÓN VÁLIDA. █▓▒░\n"
+            ;;
+    esac
+done
+
+sleep 3
+
+if [[ ! -f /var/lib/AccountsService/users/$(whoami) ]]; then
+sudo tee /var/lib/AccountsService/users/$(whoami) > /dev/null <<EOF
+[User]
+Icon=/var/lib/AccountsService/icons/$(whoami)
+SystemAccount=false
+EOF
+fi
+
+read -rp "¿QUÉ TEMA PREFIERES? CLARO (1) U OSCURO (2): " theme
+echo
+
+case "$theme" in
+  1)
+    gsettings set org.gnome.desktop.interface color-scheme 'default'
+    echo "░▒▓█ TEMA CLARO SELECCIONADO. █▓▒░\n"
+    ;;
+  2)
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    echo "░▒▓█ TEMA OSCURO SELECCIONADO. █▓▒░\n"
+    ;;
+  *)
+    echo "░▒▓█ OPCIÓN NO VÁLIDA. █▓▒░\n"
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    ;;
+esac
+
+cs=$(gsettings get org.gnome.desktop.interface color-scheme | tr -d \')
+
+if [[ "$cs" == "default" || "$cs" == "prefer-light" ]]; then
+    SCHEME="picture-uri"
+elif [[ "$cs" == "prefer-dark" ]]; then
+    SCHEME="picture-uri-dark"
+else
+    SCHEME="picture-uri-dark"
+fi
+
+echo -e "¿QUÉ COLOR DE SISTEMA PREFIERES? "
+echo -e "\n 1) Azul      2) Cian     3) Verde"
+echo -e " 4) Amarillo  5) Naranja  6) Rojo"
+echo -e " 7) Rosa      8) Morado   9) Gris\n"
+read -rp "SELECCIONA [1-9]: " accent
+echo
+
+case "$accent" in
+  1)
+    gsettings set org.gnome.desktop.interface accent-color blue || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_azul.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_azul.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\133m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR AZUL. █▓▒░\n"
+    ;;
+  2)
+    gsettings set org.gnome.desktop.interface accent-color teal || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_cian.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_cian.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\16m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR CIAN. █▓▒░\n"
+    ;;
+  3)
+    gsettings set org.gnome.desktop.interface accent-color green || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_verde.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_verde.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\177m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR VERDE. █▓▒░\n"
+    ;;
+  4)
+    gsettings set org.gnome.desktop.interface accent-color yellow || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_amarillo.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_amarillo.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\111m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR AMARILLO. █▓▒░\n"
+    ;;
+  5)
+    gsettings set org.gnome.desktop.interface accent-color orange || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_naranja.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_naranja.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1202m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR NARANJA. █▓▒░\n"
+    ;;
+  6)
+    gsettings set org.gnome.desktop.interface accent-color red || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_rojo.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_rojo.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1197m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR ROJO. █▓▒░\n"
+    ;;
+  7)
+    gsettings set org.gnome.desktop.interface accent-color pink || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_rosa.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_rosa.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1206m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR ROSA. █▓▒░\n"
+    ;;
+  8)
+    gsettings set org.gnome.desktop.interface accent-color purple || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_morado.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_morado.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1135m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR MORADO. █▓▒░\n"
+    ;;
+  9)
+    gsettings set org.gnome.desktop.interface accent-color slate || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_gris.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_gris.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1250m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    echo -e "░▒▓█ SELECCIONADO COLOR GRIS. █▓▒░\n"
+    ;;
+  *)
+    echo -e "░▒▓█ OPCIÓN NO VÁLIDA. █▓▒░\n"
+    gsettings set org.gnome.desktop.interface accent-color blue || true
+    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_azul.png"
+    sudo cp "/opt/gnome-minimal/imagenes/avatar_azul.png" "/var/lib/AccountsService/icons/$(whoami)"
+    sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1133m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
+    ;;
+esac
+
+echo -e "░▒▓█ EL SISTEMA SE REINICIARÁ (3/3) EN: █▓▒░\n"; sleep 1
+echo -e "\n░▒▓█ 3... █▓▒░\n"; sleep 1
+echo -e "\n░▒▓█ 2... █▓▒░\n"; sleep 1
+echo -e "\n░▒▓█ 1... █▓▒░\n"; sleep 1
+systemctl reboot
