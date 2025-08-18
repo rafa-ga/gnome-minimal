d2a7413dec01a5524af624bfaa27980316ddb97f
n40k1 <41051597+rafa-ga@users.noreply.github.com>
2025-08-18 12:08:22 +0200

    Update theme_switcher.sh

--- scripts/theme_switcher.sh
+++ scripts/theme_switcher.sh
@@ -37,52 +37,62 @@ read -rp "SELECCIONA [1-9]: " accent
 case "$accent" in
   1)
     gsettings set org.gnome.desktop.interface accent-color blue || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_azul.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_azul.png"
+    cp "/usr/share/pixmaps/faces/avatar_azul.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\133m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   2)
     gsettings set org.gnome.desktop.interface accent-color teal || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_cian.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_cian.png"
+    cp "/usr/share/pixmaps/faces/avatar_cian.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\16m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   3)
     gsettings set org.gnome.desktop.interface accent-color green || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_verde.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_verde.png"
+    cp "/usr/share/pixmaps/faces/avatar_verde.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\177m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   4)
     gsettings set org.gnome.desktop.interface accent-color yellow || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_amarillo.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_amarillo.png"
+    cp "/usr/share/pixmaps/faces/avatar_amarillo.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\111m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   5)
     gsettings set org.gnome.desktop.interface accent-color orange || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_naranja.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_naranja.png"
+    cp "/usr/share/pixmaps/faces/avatar_naranja.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1202m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   6)
     gsettings set org.gnome.desktop.interface accent-color red || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_rojo.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_rojo.png"
+    cp "/usr/share/pixmaps/faces/avatar_rojo.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1197m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   7)
     gsettings set org.gnome.desktop.interface accent-color pink || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_rosa.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_rosa.png"
+    cp "/usr/share/pixmaps/faces/avatar_rosa.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1206m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   8)
     gsettings set org.gnome.desktop.interface accent-color purple || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_morado.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_morado.png"
+    cp "/usr/share/pixmaps/faces/avatar_morado.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1135m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   9)
     gsettings set org.gnome.desktop.interface accent-color slate || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_gris.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_gris.png"
+    cp "/usr/share/pixmaps/faces/avatar_gris.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1250m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
   *)
     gsettings set org.gnome.desktop.interface accent-color blue || true
-    gsettings set org.gnome.desktop.background "$SCHEME" "file:///opt/gnome-minimal/imagenes/wallpaper_azul.png"
+    gsettings set org.gnome.desktop.background "$SCHEME" "file://$HOME/.local/share/backgrounds/wallpaper_azul.png"
+    cp "/usr/share/pixmaps/faces/avatar_azul.png" "/var/lib/AccountsService/icons/$(whoami)"
     sed -i -E '/PS1="/ s/(\\e\[38;5;)[0-9]{1,3}m/\1133m/g' "$HOME/.bashrc" && source "$HOME/.bashrc"
     ;;
 esac
