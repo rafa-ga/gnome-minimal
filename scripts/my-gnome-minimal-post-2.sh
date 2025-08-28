c90c608a2008bae8a6891e7e6993e2d046b63190
n40k1 <41051597+rafa-ga@users.noreply.github.com>
2025-08-28 13:29:59 +0200

    Update my-gnome-minimal-post-2.sh

--- scripts/my-gnome-minimal-post-2.sh
+++ scripts/my-gnome-minimal-post-2.sh
@@ -292,8 +292,14 @@ echo -e "░▒▓█ SCRIPTS DE INSTALACIÓN ELIMINADOS. █▓▒░\n"; sleep
 sudo sed -i '/^AutomaticLogin/ s/^/#/' /etc/gdm/custom.conf
 echo -e "░▒▓█ LOGIN AUTOMÁTICO PARA '$(whoami)' DESHABILITADO. █▓▒░\n"; sleep 1
 
-sudo systemctl disable systemd-networkd
-sudo systemctl disable systemd-resolved
+sudo mkdir -p /etc/NetworkManager/conf.d
+echo -e "[device]\nwifi.backend=iwd" | sudo tee /etc/NetworkManager/conf.d/wifi-backend.conf
+sudo systemctl disable --now wpa_supplicant.service
+sudo systemctl mask wpa_supplicant.service
+echo -e "░▒▓█ DESHABILITADO 'wpa_supplicant' Y HABILITADO 'iwd'. █▓▒░\n"; sleep 1
+
+sudo systemctl disable --now systemd-networkd.service systemd-networkd.socket systemd-networkd-wait-online.service systemd-resolved.service
+sudo systemctl mask systemd-networkd.service systemd-networkd.socket systemd-networkd-wait-online.service systemd-resolved.service
 echo -e "░▒▓█ DESHABILITADOS 'systemd-networkd' Y 'systemd-resolved'. █▓▒░\n"; sleep 1
 
 sudo sed -i '${/^.*NOPASSWD:.*$/d}' /etc/sudoers
