4edd4433d59a201ed01f3e4db0970875bc27b420
n40k1 <41051597+rafa-ga@users.noreply.github.com>
2025-09-08 11:18:21 +0200

    Update my-gnome-minimal-post-2.sh

--- scripts/my-gnome-minimal-post-2.sh
+++ scripts/my-gnome-minimal-post-2.sh
@@ -37,7 +37,7 @@ echo
 
 case "$flat" in
   1)
-    pacman -S --noconfirm flatpak
+    sudo pacman -S --noconfirm flatpak
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 	echo -e "░▒▓█ REPOSITORIO 'FlatHub' AÑADIDO. █▓▒░"; sleep 1
 
